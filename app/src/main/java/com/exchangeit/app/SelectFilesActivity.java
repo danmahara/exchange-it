fa9456524d37d0b6acd881276bcb9b2ef3a5ca3e
Dan Mahara <dan.mahara.0909@gmail.com>
2025-03-11 15:26:38 +0545

    Added new activity and navigate from main activity

--- app/src/main/java/com/exchangeit/app/SelectFilesActivity.java
+++ app/src/main/java/com/exchangeit/app/SelectFilesActivity.java
@@ -1,8 +1,7 @@
 package com.exchangeit.app;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,21 +9,19 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class SelectFilesActivity extends AppCompatActivity {
+
+    TextView app_list, image_list;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_select_files);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-    }
-    public void sendFile(View view){
-        Intent intent=new Intent(MainActivity.this,SelectFilesActivity.class);
-        startActivity(intent);
     }
 }
