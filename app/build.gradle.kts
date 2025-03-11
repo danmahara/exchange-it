44c733b7bb35b7b78102c145721fba8620e82ef3
Dan Mahara <dan.mahara.0909@gmail.com>
2025-03-11 11:23:19 +0545

    Implement UI for main activity

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,7 +8,7 @@ android {
 
     defaultConfig {
         applicationId = "com.exchangeit.app"
-        minSdk = 32
+        minSdk = 31
         targetSdk = 35
         versionCode = 1
         versionName = "1.0"
